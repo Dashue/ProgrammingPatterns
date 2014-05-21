8db2ba345cc6d9a9739920e2e7cac9258834b5f4
Johan Nilsson <jonh04@gmail.com>
2014-05-21 09:32:33 -0400

    Dynamic Dispatch

---
+++ DynamicDispatch/DynamicDispatchTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace DynamicDispatch
+{
+    public class DynamicDispatchTests
+    {
+        [Fact]
+        public void Run()
+        {
+            var dispatcher = new Dispatcher();
+
+            dispatcher.Dispatch(new A());
+            dispatcher.Dispatch(new B());
+            dispatcher.Dispatch(new C());
+        }
+    }
+}
